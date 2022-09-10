210ccfaa0063a2a9960df54f00c4141db5138e40
Abby Bangser <bangser.a@gmail.com>
2022-09-10 16:05:28 +0100

    quote around command

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 false
-
 status_code=$?
 
 BUILDEVENT_FILE=./${SERVICE}_specific_buildevents_file.txt
@@ -12,3 +11,5 @@ ci.tests.build.errors=$(xpath -q -e 'failsafe-summary/errors/text()'  ${SERVICE}
 ci.tests.build.failures=$(xpath -q -e 'failsafe-summary/failures/text()'  ${SERVICE}/target/failsafe-reports/failsafe-summary.xml)
 ci.tests.build.skipped=$(xpath -q -e 'failsafe-summary/skipped/text()'  ${SERVICE}/target/failsafe-reports/failsafe-summary.xml)
 EOF
+
+exit $status_code
